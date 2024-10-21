72173d185c956a618ae0686e36c3c7b38d022a83
EmineKarabolat <emine.karabolat.01@gmail.com>
2024-10-21 14:22:16 +0300

    first commit

--- src/main/java/org/example/eticaretapp/ETicaretAppApplication.java
+++ src/main/java/org/example/eticaretapp/ETicaretAppApplication.java
@@ -8,6 +8,7 @@ public class ETicaretAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ETicaretAppApplication.class, args);
+        System.out.println("Hello World!");
     }
 
 }
