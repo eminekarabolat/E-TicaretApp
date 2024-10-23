560bf7f844fe5b2ca4872e6919de6b13d1db357b
msacak <msacakk@gmail.com>
2024-10-23 15:53:43 +0300

    Sepet uygulamasına başlandı.

--- src/main/java/org/example/eticaretapp/repository/ShoppingCartRepository.java
+++ src/main/java/org/example/eticaretapp/repository/ShoppingCartRepository.java
@@ -3,9 +3,11 @@ package org.example.eticaretapp.repository;
 import org.example.eticaretapp.entity.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart,Long> {
 
-    ShoppingCart find
-
+    Optional<ShoppingCart> findByUserIdAndDone(Long userId,Boolean isDone);
 
 }
--- src/main/java/org/example/eticaretapp/service/ShoppingCartService.java
+++ src/main/java/org/example/eticaretapp/service/ShoppingCartService.java
@@ -3,18 +3,39 @@ package org.example.eticaretapp.service;
 import lombok.RequiredArgsConstructor;
 import org.example.eticaretapp.dto.request.AddProductToShoppingCartDto;
 import org.example.eticaretapp.entity.CartDetails;
+import org.example.eticaretapp.entity.ShoppingCart;
+import org.example.eticaretapp.exception.ETicaretException;
+import org.example.eticaretapp.exception.ErrorType;
 import org.example.eticaretapp.repository.ShoppingCartRepository;
+import org.example.eticaretapp.utility.JwtManager;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ShoppingCartService {
 
     private final ShoppingCartRepository shoppingCartRepository;
+    private JwtManager jwtManager;
 
 
     public void addProductToShoppingCart(AddProductToShoppingCartDto dto) {
-        dto.token()
+        Optional<Long> userIdOpt = jwtManager.validateToken(dto.token());
+        if(userIdOpt.isEmpty()) throw new ETicaretException(ErrorType.INVALID_TOKEN);
 
+        Optional<ShoppingCart> shoppingCartOptional = shoppingCartRepository.findByUserIdAndDone(userIdOpt.get(), false);
+        if(shoppingCartOptional.isPresent()) {
+            ShoppingCart shoppingCart = shoppingCartOptional.get();
+
+            CartDetails cartDetails = CartDetails.builder()
+                    .build()
+        }
+        else{
+            ShoppingCart shoppingCart = ShoppingCart.builder()
+                    .userId(userIdOpt.get())
+                    .build();
+            shoppingCartRepository.save(shoppingCart);
+        }
     }
 }
